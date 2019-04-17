4788a943c2ee5af35f75e092670b6c3da93c09d0
Rodrigues de Souza <jars@gft.com>
2019-04-17 19:16:30 -0300

    create User class model MVC

---
+++ models/User.js
@@ -0,0 +1,16 @@
+class User {
+
+    constructor (name, gender, birth, coutry, email, password, photo, admin){
+
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+        this.coutry = coutry;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;
+
+    }
+
+}
